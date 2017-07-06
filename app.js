772d340637dc73bb307ad826ad1e12f83d83f812
fajarkarim <fajarabdulkarim@gmail.com>
2017-07-06 10:14:30 +0700

    configured app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose')
+var cors = require('cors')
+var url = "mongodb://localhost/fajar-simple-api-crud"
+mongoose.connect(url);
+
+
+var foods = require('./routes/foods');
+var restaurants = require('./routes/restaurants')
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
+
+app.use('/api/foods', foods)
+app.use('/api/restaurants', restaurants);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+module.exports = app;
